8050d902231f5cacc7d726bdd86a460e9ac82003
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-12-12 09:05:06 +0300

    Add Challenge 43

--- script.js
+++ script.js
@@ -80,3 +80,33 @@ function anagrams(str, arr) {
 // console.log(anagrams("abba", ["aabb", "abcd", "bbaa", "dada"]));
 // console.log(anagrams("racer", ["crazer", "carer", "racar", "caers", "racer"]));
 // console.log(anagrams("laser", ["lazing", "lazy", "lacer"]));
+
+// 43. Convert HTML Entities - freecodecamp
+/* 
+Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.
+
+********************************
+convertHTML("Dolce & Gabbana") should return the string Dolce &amp; Gabbana.
+
+convertHTML("Hamburgers < Pizza < Tacos") should return the string Hamburgers &lt; Pizza &lt; Tacos.
+
+convertHTML("Sixty > twelve") should return the string Sixty &gt; twelve.
+
+convertHTML('Stuff in "quotation marks"') should return the string Stuff in &quot;quotation marks&quot;.
+
+convertHTML("Schindler's List") should return the string Schindler&apos;s List.
+
+convertHTML("<>") should return the string &lt;&gt;.
+
+convertHTML("abc") should return the string abc.
+ */
+function convertHTML(str) {
+    return str
+        .replaceAll("&", "&amp;")
+        .replaceAll("<", "&lt;")
+        .replaceAll(">", "&gt;")
+        .replaceAll('"', "&quot;")
+        .replaceAll("'", "&apos;");
+}
+
+console.log(convertHTML("Dolce & Gabbana"));
