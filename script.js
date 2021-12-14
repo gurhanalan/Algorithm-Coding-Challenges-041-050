1bddd378b5567acef5a798dee8328dc730675585
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-12-14 09:07:49 +0300

    Add Challenge 45

--- script.js
+++ script.js
@@ -147,3 +147,31 @@ function sumFibs(num) {
 }
 
 console.log(sumFibs(75025));
+
+// 45. Sum All Primes - freecodecamp
+/* 
+A prime number is a whole number greater than 1 with exactly two divisors: 1 and itself. For example, 2 is a prime number because it is only divisible by 1 and 2. In contrast, 4 is not prime since it is divisible by 1, 2 and 4.
+
+Rewrite sumPrimes so it returns the sum of all prime numbers that are less than or equal to num.
+
+********************************
+sumPrimes(10) should return a number.
+
+sumPrimes(10) should return 17.
+
+sumPrimes(977) should return 73156.
+ */
+
+function sumPrimes(num) {
+    const arrPrimes = [2];
+    let i = 2;
+    while (i <= num) {
+        if (arrPrimes.every((el) => i % el !== 0)) arrPrimes.push(i);
+
+        i++;
+    }
+
+    return arrPrimes.reduce((acc, cur) => acc + cur);
+}
+
+console.log(sumPrimes(2));
